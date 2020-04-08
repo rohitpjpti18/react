eee7874770d116830cbc70732ed1e503c0748930
rohitpjpti18 <rohitpjpti18@gmail.com>
2020-04-08 10:02:32 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react
 react-practices
+
+
+`npm install`
---
+++ revise/src/components/navBar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function NavBar() {
+    return (
+      <div>
+        <p>Home</p>
+        <p>About</p>
+        <p>Products</p>
+      </div>
+    );
+  }
+  
+  export default NavBar;
