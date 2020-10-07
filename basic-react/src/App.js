377323effc715494ebc9e80fe76efd6864b7af50
Rohit Prajapati <51499109+rohitpjpti18@users.noreply.github.com>
2020-10-07 19:33:20 +0530

    revisting react: day 1

--- basic-react/src/App.js
+++ basic-react/src/App.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import MyInfo from './components/MyInfo';
-import MainPage from './components/MainPage';
 
 function App() {
   return (
-    <div>
-      <MyInfo />
-      <MainPage />
-    </div>
+    <MyInfo />
   );
 }
 
---
+++ basic-react/src/components/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function MyInfo(){
+    return(
+        <div>
+            <h1>Rohit Prajapati</h1>
+            <p>Back to basic mode</p>
+            <ul>
+                <li>Guitar</li>
+                <li>Monitor 27 inch 4k</li>
+                <li>Mechanical Keyboard.</li>
+            </ul>
+        </div>
+    );
+}
+
+export default MyInfo;
